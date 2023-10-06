ccb71828bdeee801faa35d31b2edb24cb10e82c8
s.krotov <14bassdrumand88@gmail.com>
2023-10-06 13:25:29 +0300

    1 screens + topBar

---
+++ app/src/main/java/com/kenig/notesapp/screens/AddNoteScreen.kt
@@ -0,0 +1,12 @@
+package com.kenig.notesapp.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun AddNoteScreen(navHostController: NavHostController) {
+    Text(
+        text = "Splash Screen"
+    )
+}
---
+++ app/src/main/java/com/kenig/notesapp/screens/SplashScreen.kt
@@ -0,0 +1,13 @@
+package com.kenig.notesapp.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun SplashScreen(navHostController: NavHostController) {
+    Text(
+        text = "Splash Screen"
+    )
+
+}
